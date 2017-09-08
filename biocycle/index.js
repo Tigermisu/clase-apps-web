0fddfb999ac7b27ae37724aa6f45844582317eac
Unknown <tigermisu@outlook.com>
2017-09-07 18:40:43 -0600

    store and get users

--- biocycle/index.js
+++ biocycle/index.js
@@ -7,10 +7,8 @@ const serveStatic = require('serve-static')
 const app = express();
 const ws = expressWs(app);
 
-const dbaddress = "mongodb://ClaseAppsWeb:<sistemasoperativos>@claseappsweb-shard-00-00-4ukh5.mongodb.net:27017,claseappsweb-shard-00-01-4ukh5.mongodb.net:27017,claseappsweb-shard-00-02-4ukh5.mongodb.net:27017/test?ssl=true&replicaSet=ClaseAppsWeb-shard-0&authSource=admin"
+const dbaddress = "mongodb://ClaseAppsWeb:sistemasoperativos@claseappsweb-shard-00-00-4ukh5.mongodb.net:27017,claseappsweb-shard-00-01-4ukh5.mongodb.net:27017,claseappsweb-shard-00-02-4ukh5.mongodb.net:27017/bioritmo?ssl=true&replicaSet=ClaseAppsWeb-shard-0&authSource=admin"
 var MongoClient = require('mongodb').MongoClient;
-
-
 
 log4js.configure({
   appenders: {
@@ -28,31 +26,31 @@ app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.ws('/ws', (ws, req) => {
   logger.debug("Received new websocket connection.");
-//do query base de datos de usuarios
-  MongoClient.connect(dbaddress, function(err, db) {
-  console.log("Connected correctly to server.");
-  console.log(err);
-  db.collection('people').find({}).then(function(e){
-	  console.log(e)
+
+  MongoClient.connect(dbaddress, function (err, db) {
+    logger.debug(err);
+    getUsers(db, (users) => {
+      ws.send(JSON.stringify({type: "users", payload: users}));
+      logger.debug("Finished querying users");
+    });
+    db.close();
   });
-  db.close();
-});
-  
-  
+
+
   ws.on('message', (msg) => {
     logger.debug("Received message from WS:", msg);
     try {
       var json = JSON.parse(msg),
         dia1 = new Date;
-        dia2 = new Date(json.nacimiento);
-		name = json.name;
-      
+      dia2 = new Date(json.nacimiento);
+      name = json.name;
+
       var oneDay = 24 * 60 * 60 * 1000;
       var diffDays = Math.round(Math.abs((dia1.getTime() - dia2.getTime()) / (oneDay)));
       var fisico = diffDays % 23;
       var emocional = diffDays % 28;
       var intelectual = diffDays % 33;
-      
+
       logger.debug(dia1);
       logger.debug(dia2);
       logger.debug(diffDays);
@@ -60,16 +58,40 @@ app.ws('/ws', (ws, req) => {
       logger.debug(intelectual);
       logger.debug(emocional);
 
-      ws.send(JSON.stringify({diffDays: diffDays, fisico: fisico, intelectual: intelectual, emocional: emocional}));
+      ws.send(JSON.stringify({type: "bioritmo", payload: {
+        diffDays: diffDays,
+        fisico: fisico,
+        intelectual: intelectual,
+        emocional: emocional
+      }}));
 
-	  //mandar query para guardar db
-		MongoClient.connect(dbaddress, function(err, db) {
-  console.log("Connected correctly to server.");
-  db.collection('people').insertMany([{name: name, birthday: dia2, diffDays: diffDays, fisico: fisico, intelectual: intelectual, emocional: emocional}]).then(function(result) {console.log("Pushed person!", result)})
-  db.close();
-});
-	
-    } catch(e) {
+      //mandar query para guardar db
+      MongoClient.connect(dbaddress, function (err, db) {
+        db.collection('people').insertOne({
+          name: name,
+          birthday: dia2,
+          diffDays: diffDays,
+          fisico: fisico,
+          intelectual: intelectual,
+          emocional: emocional
+        }).then(function (result) {
+          logger.debug("Pushed person " + name);
+
+          MongoClient.connect(dbaddress, function (err, db) {
+            logger.debug("Connected correctly to server.");
+            logger.debug(err);
+            getUsers(db, (users) => {
+              ws.send(JSON.stringify({type: "users", payload: users}));
+              logger.debug("Finished querying users");
+            });
+            db.close();
+          });
+        })
+
+        db.close();
+      });
+
+    } catch (e) {
       logger.error("Failed to parse JSON:", e);
     }
   });
@@ -82,3 +104,18 @@ app.ws('/ws', (ws, req) => {
 app.listen(3000, () => {
   logger.info('App listening on port 3000');
 });
+
+function getUsers(db, callback) {
+  var cursor = db.collection('people').find();
+  let people = []
+  cursor.each(function(err, doc) {
+     if(err != null) {
+       throw err;
+     }
+     if (doc != null) {
+        people.push(doc);
+     } else {
+        callback(people);
+     }
+  });
+};
--- biocycle/public/app.js
+++ biocycle/public/app.js
@@ -1,74 +1,97 @@
 var ws = new WebSocket("ws://localhost:3000/ws");
 
 ws.onopen = function (event) {
-	console.info("Web socket is open!", event);
+  console.info("Web socket is open!", event);
 };
 
 ws.onmessage = function (event) {
-	console.info("Received ws message from the server.");
-	var message = event.data;
-	console.log(message);
+  console.info("Received ws message from the server.");
+  var message = JSON.parse(event.data);
+  console.log(message);
 
-	try {
-		var json = JSON.parse(message),
-			diffDays = json.diffDays,
-			fisico = json.fisico,
-			emocional = json.emocional,
-			intelectual = json.intelectual;
+  if (message.type == "bioritmo") {
+	var json = message.payload;
+    try {
+        var diffDays = json.diffDays,
+        fisico = json.fisico,
+        emocional = json.emocional,
+        intelectual = json.intelectual;
 
 
-		$("#vividos").text('Dias vividos a la fecha de calculo: ' + diffDays);
-		$("#fisico").text('Dias del Ciclo fisico (23): ' + fisico);
-		$("#emocional").text('Dias del Ciclo Emocional (28): ' + emocional);
-		$("#intelectual").text('Dias del Ciclo Intelectual (33): ' + intelectual);
-	
-		//cambia el color del ciclo fisico
-		if (3 <= fisico && fisico <= 9) {
-			$("#fisico").css("color", "green");
-		} else if ((0 <= fisico && fisico <= 3) || (9 <= fisico && fisico <= 14) || (20 <= fisico && fisico <= 23)) {
-			$("#fisico").css("color", "#f1c40f");
-		} else if (14 <= fisico && fisico <= 20) {
-			$("#fisico").css("color", "red");
-		}
-	
-		//cambia el color del ciclo emocional
-		if (3 <= emocional && emocional <= 11) {
-			$("#emocional").css("color", "green");
-		} else if ((0 <= emocional && emocional <= 3) || (11 <= emocional && emocional <= 17) || (25 <= emocional && emocional <= 28)) {
-			$("#emocional").css("color", "#f1c40f");
-		} else if (17 <= emocional && emocional <= 25) {
-			$("#emocional").css("color", "red");
-		}
-	
-		//cambia el color del ciclo intelectual
-		if (4 <= intelectual && intelectual <= 12) {
-			$("#intelectual").css("color", "green");
-		} else if ((0 <= intelectual && intelectual <= 4) || (12 <= intelectual && intelectual <= 21) || (29 <= intelectual && intelectual <= 33)) {
-			$("#intelectual").css("color", "#f1c40f");
-		} else if (21 <= intelectual && intelectual <= 29) {
-			$("#intelectual").css("color", "red");
-		}
-	} catch(e) {
-		console.warn("Failed to parse message from json", e);
-	}	
+      $("#vividos").text('Dias vividos a la fecha de calculo: ' + diffDays);
+      $("#fisico").text('Dias del Ciclo fisico (23): ' + fisico);
+      $("#emocional").text('Dias del Ciclo Emocional (28): ' + emocional);
+      $("#intelectual").text('Dias del Ciclo Intelectual (33): ' + intelectual);
+
+      //cambia el color del ciclo fisico
+      if (3 <= fisico && fisico <= 9) {
+        $("#fisico").css("color", "green");
+      } else if ((0 <= fisico && fisico <= 3) || (9 <= fisico && fisico <= 14) || (20 <= fisico && fisico <= 23)) {
+        $("#fisico").css("color", "#f1c40f");
+      } else if (14 <= fisico && fisico <= 20) {
+        $("#fisico").css("color", "red");
+      }
+
+      //cambia el color del ciclo emocional
+      if (3 <= emocional && emocional <= 11) {
+        $("#emocional").css("color", "green");
+      } else if ((0 <= emocional && emocional <= 3) || (11 <= emocional && emocional <= 17) || (25 <= emocional && emocional <= 28)) {
+        $("#emocional").css("color", "#f1c40f");
+      } else if (17 <= emocional && emocional <= 25) {
+        $("#emocional").css("color", "red");
+      }
+
+      //cambia el color del ciclo intelectual
+      if (4 <= intelectual && intelectual <= 12) {
+        $("#intelectual").css("color", "green");
+      } else if ((0 <= intelectual && intelectual <= 4) || (12 <= intelectual && intelectual <= 21) || (29 <= intelectual && intelectual <= 33)) {
+        $("#intelectual").css("color", "#f1c40f");
+      } else if (21 <= intelectual && intelectual <= 29) {
+        $("#intelectual").css("color", "red");
+      }
+    } catch (e) {
+      console.warn("Failed to parse message from json", e);
+    }
+  } else if (message.type == "users") {
+	  $('table tbody').html('');
+	  message.payload.forEach((user) => {
+		var html = "<tr><td>",
+			bday = new Date(user.birthday),
+			month = bday.getMonth() + 1;
+			month = month < 10 ? "0" + month : month;
+			bday = bday.getDate() + "-" + month + "-" + bday.getFullYear(); 
+
+		html += user.name + "</td><td>";
+		html += bday + "</td><td>";
+		html += user.diffDays + "</td><td>";
+		html += user.fisico + "</td><td>";
+		html += user.emocional + "</td><td>";
+		html += user.intelectual + "</td></tr>";
+
+		$('table tbody').append(html);
+	});
+  }
 }
 
 ws.onclose = function () {
-	console.warn("Web socket is closing:", ws);
+  console.warn("Web socket is closing:", ws);
 }
 
 $(document).ready(function () {
-	var today = new Date(),
-		month = today.getMonth() + 1;
-	month = month < 10 ? "0" + month: month;
-	today = today.getFullYear() + "-" + month + "-" + today.getDate();
-	$('#today').val(today);
+  var today = new Date(),
+    month = today.getMonth() + 1;
+  month = month < 10 ? "0" + month : month;
+  today = today.getFullYear() + "-" + month + "-" + today.getDate();
+  $('#today').val(today);
 
-	$("#submit").click(function () {
-		var name = document.getElementById("name").value;
-		var dia2 = new Date(document.getElementById("birthday").value);
+  $("#submit").click(function () {
+    var name = document.getElementById("name").value;
+    var dia2 = new Date(document.getElementById("birthday").value);
 
-		ws.send(JSON.stringify({nacimiento: dia2, name: name}));
+    ws.send(JSON.stringify({
+      nacimiento: dia2,
+      name: name
+    }));
 
-	});
+  });
 });
